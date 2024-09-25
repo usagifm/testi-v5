860aaa2caca37f555c1e5bf2597fa4101289c9d2
Muhammad Nur Faqqih <muhammad.faqqih@erajaya.com>
2024-09-25 14:37:24 +0700

    a

--- main.js
+++ main.js
@@ -4,7 +4,21 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Cuma mau bilang", "Selamat Sore Sayangku", "Aku Sayang Banget Sama Kamu", "I Love You Annisa Aulia"],
+        strings: ["Dear Mutia Alima Ayu Arlendi", 
+            "Kamu inget ga pertama kali kita kenal ? iyaa sore sore ituu, yang aku kira hanya akan sebatas teman seperti itu ajaa", 
+            "Aku dulu memang tidak mau untuk menjalin hubungan yang 'erat' apalagi sampai bermain perasaan untuk hubungan yang awalnya hanya akan untuk 'itu itu saja'", 
+            "Tapi, ternyata banyak ya yang udah terjadi, mungkin udah gabisa diceritain lagi dengan kata kata yang ada di keyboard ku ini bib",
+            "Banyak yang menyenangkan dan membahagiakan, tapi tidak luput juga yang sakit dan menyedihkan, bukan kah kamu setuju bib ?",
+            "Namun, itu semua membuat kita sama sama berkembang dan menjadi lebih dewasa dan lebih baik lagi, kejadian demi kejadian, kenangan demi kenangan membangun hubungan kita yang sekarang ini",
+            "Aku bersyukur bib bisa kenal dengan kamu, dan aku harap kamu pun begitu",
+            "Kamu tu bib, kaya sebuah emas yang selalu ngira kalau dirinya itu sebuah kuningan, yang kadang ngerasa tidak seberharga itu 😞",
+            "Pada kenyataannya, kamu sangat sangat berharga bib, Kamu cantik, manis, putih, badan mu bagus, bibir mu manis, badan mu wangi, aahh banyak banget kelebihan mu bib secara fisik",
+            "Tapi bukan cuma itu yang menjadikan berharga bib, karena pada sejatinya harga diri kita ada pada value kita sebagai seseorang manusia, bagaimana prilaku, sifat dan kecerdasan kita",
+            "Aku udah ga terhitung berapa kali amaze dengan kemahiran kamu dalam menghafal dan mempelajari tentang obat, berkomunikasi, mengajari pelanggan, bagaimana menghandle emosi dengan pelanggan dann masih banyak lagi",
+            "Yang terpenting lagi, yaitu bagaimana kamu hidup sekarang sebagai manusia yang baik, tidak merugikan orang lain, bisa berbakti sama mama dan abang, terus berkembang dengan kuliah, dan punya cita cita yang tinggi bib",
+            "Itu semua yang membuat kamu berharga Mutia, kalau kamu tidak merasa seperti itu, tolong lah mengerti bahwa aku merasa seperti itu",
+            "Kayaknya udah kepanjangan ya ocehan ku ini bib hahahaha",
+            "Yang jelas, I Love You Mutia Alima Ayu Arlendi"],
         typeSpeed: 70
     });
 }
